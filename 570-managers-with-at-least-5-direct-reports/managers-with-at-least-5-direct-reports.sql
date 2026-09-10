463970fd6d136c09ec8f1f239bb4a385dc4fb068
Aditya-Bala-45 <adityabala3605@gmail.com>
2026-09-11 01:02:41 +0530

    Time: 347 ms (84.34%) | Memory: 0B (100.00%) - LeetSync

--- 570-managers-with-at-least-5-direct-reports/managers-with-at-least-5-direct-reports.sql
+++ 570-managers-with-at-least-5-direct-reports/managers-with-at-least-5-direct-reports.sql
@@ -1,9 +1,14 @@
--- Write your PostgreSQL query statement below
-WITH cte AS(
-Select e.managerId,s.name,count(e.id) as direct from Employee as e
-join Employee as s on e.managerId=s.id
-where e.managerId is NOT NULL 
-group by e.managerId,s.name)
-SELECT name from cte
-where direct >= 5
-;
+WITH cte AS (
+    SELECT 
+        managerId,
+        COUNT(managerId) AS C
+    FROM Employee
+    GROUP BY managerId
+    HAVING COUNT(managerId) >= 5
+       AND managerId IS NOT NULL
+)
+
+SELECT f.name
+FROM cte
+INNER JOIN Employee f
+    ON cte.managerId = f.id;
