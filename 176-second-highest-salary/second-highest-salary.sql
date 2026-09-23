7f5f99dadd994d103c2a58576647b904324be01c
Aditya-Bala-45 <adityabala3605@gmail.com>
2026-09-23 14:53:31 +0530

    Time: 204 ms (60.78%) | Memory: 0B (100.00%) - LeetSync

---
+++ 176-second-highest-salary/second-highest-salary.sql
@@ -0,0 +1,8 @@
+-- Write your PostgreSQL query statement below
+SELECT MAX(salary) SecondHighestSalary
+FROM
+(SELECT id,salary,
+DENSE_RANK() OVER (ORDER BY salary DESC) AS rnk
+FROM Employee)t
+WHERE rnk=2
+;
